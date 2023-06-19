6211b067e14bb0c52281d16e6cb7468eaf869ba6
Maxime Croisier <crmaxime@gmail.com>
2023-06-19 17:07:34 +0200

    add swagger doc

--- app.js
+++ app.js
@@ -7,15 +7,31 @@ const app = express()
 const cors = require('cors')
 const port = 3000
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Carambar Jokes API',
+    },
+    servers: [{ url: '/api/jokes' }]
+  },
+  apis: ['./routes/*.js'], // files containing annotations as above
+};
+const swaggerSpec = swaggerJsdoc(options);
+
+
 app.use(express.json())
 app.use(cors())
 
-app.use('/jokes', jokesRouter)
+app.use('/api/jokes', jokesRouter)
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
--- routes/jokes.js
+++ routes/jokes.js
@@ -2,11 +2,66 @@ const express = require('express')
 const router = express.Router()
 const jokesController = require('../controllers/jokes')
 
-/* GET users listing. */
+/**
+ * @swagger
+ *
+ * /:
+ *   get:
+ *     tags:
+ *     - Jokes
+ *     produces:
+ *       - application/json
+ *     summary: Get all jokes
+ *     description: Get all jokes
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '404':
+ *         description: Joke not found
+ */
 router.get('/', jokesController.getJokes)
 
+/**
+ * @swagger
+ *
+ * /joke/{id}:
+ *   get:
+ *     tags:
+ *     - Jokes
+ *     produces:
+ *       - application/json
+ *     summary: Get joke by id
+ *     description: Get answer by id
+ *     parameters:
+ *     - name: id
+ *       in: path
+ *       description: ID of the joke
+ *       required: true
+ *       schema:
+ *         type: number
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ *       '404':
+ *         description: Joke not found
+ */
 router.get('/joke/:id', jokesController.getJokeById)
 
+/**
+ * @swagger
+ *
+ * /random:
+ *   get:
+ *     tags:
+ *     - Jokes
+ *     produces:
+ *       - application/json
+ *     summary: Get a random joke
+ *     description: Get a random joke
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ */
 router.get('/random', jokesController.getRandomJoke)
 
 module.exports = router

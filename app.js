4aa6fed3c4efa01a8943e9bf9e322df52647935d
Maxime Croisier <crmaxime@gmail.com>
2023-06-19 15:24:22 +0200

    change route '/'

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const path = require('path')
 
-const indexRouter = require('./routes/index')
 const jokesRouter = require('./routes/jokes')
 
 const app = express()
+const cors = require('cors')
 const port = 3000
 
 app.use(express.json())
-// app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors())
 
-app.use('/', indexRouter)
 app.use('/jokes', jokesRouter)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 
 app.listen(port, () => {
